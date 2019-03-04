directory overhaul and derivatives fixed

1Dgaussian_mean_variance/main.py
@@ -1,5 +1,7 @@
 import sys
 import numpy as np
+import matplotlib
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import keras
@@ -55,6 +57,7 @@ class nholder(object):
 		self.num_epochs = num_epochs
 		self.keep_rate = keep_rate
 		self.verbose = verbose
+		self.rescaled = False
 
 		self.data, self.der_den = self.create_data()
 		# Make parameters dictionary of params that are always the same or defined
@@ -171,6 +174,28 @@ class nholder(object):
 
 		return data, der_den
 
+	def rescale_data(self):
+		"""
+		Rescaling the input data. 
+		"""
+
+		self.rescaled = True
+
+		'''
+		# Anti standardization
+		for key in self.data.keys():
+			self.data[key] += 5
+			self.data[key] *= 3
+		'''
+
+		# Scale by subtracting the mean and dividing by std
+		mu = np.mean(self.data['x_central'])
+		sigma = np.nanstd(self.data['x_central'])
+		for key in self.data.keys():
+			self.data[key] -= mu
+			self.data[key] /= sigma
+
+
 	def plot_data(self, show=False):
 		""" 
 		Plot the data as data amplitude
@@ -217,7 +242,7 @@ class nholder(object):
 		ax[0].set_xlabel("Data amplitude")
 		ax[0].set_ylabel('Counts')
 		ax[0].set_title('%i data points'%self.input_shape[0])
-		ax[0].set_xlim(0,6)
+		if not self.rescaled: ax[0].set_xlim(0,6)
 
 		ax[1].hist(self.data['x_central_test'][np.random.randint(self.n_s)]
 			,label='test data',alpha=0.5)
@@ -226,7 +251,7 @@ class nholder(object):
 		ax[0].set_title('%i data points'%self.input_shape[0])
 		ax[1].set_xlabel("Data amplitude")
 		ax[1].set_ylabel('Counts')
-		ax[1].set_xlim(0,6)
+		if not self.rescaled: ax[1].set_xlim(0,6)
 		
 		plt.savefig(f'{self.figuredir}data_visualization_hist_{self.modelversion}.png')
 		if show: plt.show()
@@ -299,7 +324,7 @@ class nholder(object):
 		ax[1, 0].plot(self.data['x_p_test'][test_index, 0] - self.data['x_m_test'][test_index, 0]
 					, color = 'C1',alpha=0.5)
 		ax[1, 0].set_xticks([])
-		ax[1, 0].set_ylabel("Difference between derivative data amplitudes")
+		ax[1, 0].set_ylabel("Difference between upper/lower data amplitudes")
 
 		# Theta 2
 		ax[1, 1].axhline(xmin = 0., xmax = 1., y = 0., linestyle = 'dashed'
@@ -309,7 +334,7 @@ class nholder(object):
 		ax[1, 1].plot(self.data['x_p_test'][test_index, 1] - self.data['x_m_test'][test_index, 1]
 					, color = 'C1',alpha=0.5)
 		ax[1, 1].set_xticks([])
-		ax[1, 1].set_ylabel("Difference between derivative data amplitudes");
+		ax[1, 1].set_ylabel("Difference between upper/lower data amplitudes");
 
 		plt.savefig(f'{self.figuredir}derivatives_visualization_{self.modelversion}.png')
 		if show: plt.show()
@@ -334,7 +359,7 @@ class nholder(object):
 		
 		return n
 
-	def train_network(self, n):
+	def train_network(self, n, to_continue=False):
 		""" 
 		Train the created network with the given data and parameters
 		Saves the history and the determinant of the final fisher info
@@ -346,7 +371,8 @@ class nholder(object):
 		"""
 
 		n.train(num_epochs = self.num_epochs, n_train = self.n_train
-			, keep_rate = self.keep_rate, data = self.data, history = True)
+			, keep_rate = self.keep_rate, data = self.data, history = True
+			, to_continue= to_continue)
 
 		# save the network history to a file
 		utils_mrp.save_history(self, n)
@@ -363,7 +389,7 @@ class nholder(object):
 		n 						class 	IMNN class as defined in IMNN.py
 		
 		"""
-		fig, ax = plt.subplots(5, 1, sharex = True, figsize = (8, 14))
+		fig, ax = plt.subplots(6, 1, sharex = True, figsize = (8, 14))
 		plt.subplots_adjust(hspace = 0)
 		end = len(n.history["det(F)"])
 		epochs = np.arange(end)
@@ -384,29 +410,37 @@ class nholder(object):
 		ax[2].set_ylabel(r'$|{\bf C}|$')
 		ax[2].set_xlim([0, len(epochs)]);
 		
-		'''
-		# Derivative wrt to theta1				   theta1 is column 0
-		ax[3].plot(epochs, np.array(n.history["dμdθ"])[:,0].flatten()
-			, color = 'C0', label='theta1',alpha=0.5)
-		# Derivative wrt to theta2				   theta1 is column 1
-		ax[3].plot(epochs, np.array(n.history["dμdθ"])[:,1].flatten()
-			, color = 'C0', ls='dashed', label='theta2',alpha=0.5)
-
-		ax[3].plot(epochs, np.array(n.history["test dμdθ"])[:,0].flatten()
-			, color = 'C1', label='theta1',alpha=0.5)
-		ax[3].plot(epochs, np.array(n.history["test dμdθ"])[:,1].flatten()
-			, color = 'C1', ls='dashed', label='theta2',alpha=0.5)
-		ax[3].legend(frameon=False)
-
-		ax[3].set_ylabel(r'$\partial\mu/\partial\theta$')
+		
+		# Derivative of first summary wrt to theta1				   theta1 is 3rd dimension index 0
+		ax[3].plot(epochs, np.array(n.history["dμdθ"])[:,0,0]
+			, color = 'C0', label=r'$\theta_1$',alpha=0.5)
+		# Test Derivative of first summary wrt to theta1				   theta1 is 3rd dimension index 0
+		ax[3].plot(epochs, np.array(n.history["test dμdθ"])[:,0,0]
+			, color = 'C1', label=r'$\theta_1$',alpha=0.5)
+		ax[3].set_ylabel(r'$\partial\mu/\partial\theta_1$')
 		ax[3].set_xlabel('Number of epochs')
 		ax[3].set_xlim([0, len(epochs)])
-		ax[4].plot(epochs, np.array(n.history["μ"]).reshape((np.prod(np.array(n.history["μ"]).shape))),alpha=0.5)
-		ax[4].plot(epochs, np.array(n.history["test μ"]).reshape((np.prod(np.array(n.history["test μ"]).shape))),alpha=0.5)
-		ax[4].set_ylabel('μ')
+		# ax[3].legend(frameon=False)
+
+		# Derivative of first summary wrt to theta2				   theta2 is 3rd dimension index 1
+		ax[4].plot(epochs, np.array(n.history["dμdθ"])[:,0,1]
+			, color = 'C0', ls='dashed', label=r'$\theta_2$',alpha=0.5)
+		# Test Derivative of first summary wrt to theta2				   theta2 is 3rd dimension index 1
+		ax[4].plot(epochs, np.array(n.history["test dμdθ"])[:,0,1]
+			, color = 'C1', ls='dashed', label=r'$\theta_2$',alpha=0.5)
+		ax[4].set_ylabel(r'$\partial\mu/\partial\theta_2$')
 		ax[4].set_xlabel('Number of epochs')
 		ax[4].set_xlim([0, len(epochs)])
-		'''
+		# ax[4].legend(frameon=False)
+
+		# Mean of network output summary 1
+		ax[5].plot(epochs, np.array(n.history["μ"])[:,0],alpha=0.5)
+		# Mean of test output network summary 1
+		ax[5].plot(epochs, np.array(n.history["test μ"])[:,0],alpha=0.5)
+		ax[5].set_ylabel('μ_1')
+		ax[5].set_xlabel('Number of epochs')
+		ax[5].set_xlim([0, len(epochs)])
+		
 
 		print ('Maximum Fisher info on train data:',np.max(n.history["det(F)"]))
 		print ('Final Fisher info on train data:',(n.history["det(F)"][-1]))
@@ -418,6 +452,56 @@ class nholder(object):
 			print ('Promising network found, possibly more epochs needed')
 
 		plt.savefig(f'{self.figuredir}variables_vs_epochs_{self.modelversion}.png')
+		if show: plt.show()
+		plt.close()
+
+	def plot_train_output(self, n, show=False, amount=10):
+		""" 
+		Plot network output on training set vs epochs, and show the mean as well
+
+		INPUTS
+		#______________________________________________________________
+		n					   class   IMNN class as defined in IMNN.py
+		amount				  int	 how many outputs to plot
+		
+		"""
+
+		fig, axes = plt.subplots(3, 1, sharex = True, figsize = (8, 6))
+		ax = axes[0]
+
+		# track 'amount' random outputs of the 1000 input simulations
+		random_indices = np.random.randint(0,self.n_s, amount)
+		outputs = np.asarray(n.history['train output'])[:,random_indices,0] # of first summary
+		end = len(n.history['train output'])
+		epochs = np.arange(end)
+
+		# plot 'amount' random outputs vs epochs
+		for i in range(amount):
+			ax.plot(epochs,outputs[:,i],alpha=0.5,ls='dashed')
+
+		# plot the network mean (of first summary) of all the input simulations
+		ax.plot(epochs, np.array(n.history["μ"])[:,0],ls='solid',label='μ')
+		ax.set_ylabel('Network output')
+		ax.set_title(f'Output of {amount} random input simulations')
+		ax.legend()
+
+		# plot the network mean of all the input simulations, in a separate plot
+		ax = axes[1]
+		ax.plot(epochs, np.array(n.history["μ"])[:,0],ls='solid',label='μ')
+		ax.set_title(f'Network mean')
+		ax.set_ylabel('Network output')
+
+		# plot the numpy mean of this subset
+		ax = axes[2]
+		ax.set_title('numpy mean/std of the random subset of simulations')
+		ax.errorbar(epochs, np.mean(outputs,axis=1), yerr=np.std(outputs,axis=1),label=r'$1\sigma$',zorder=1)
+		ax.plot(epochs, np.mean(outputs,axis=1),label='mean',zorder=2)
+		ax.set_ylabel('Value')
+		ax.legend()
+		
+		axes[-1].set_xlabel('Epoch')
+		plt.tight_layout()
+		plt.savefig(f'{self.figuredir}output_vs_epochs_{self.modelversion}.png')
 		if show: plt.show()
 		plt.close()
 
@@ -455,9 +539,12 @@ class nholder(object):
 
 		# Draws are accepted if the distance between the simulation summary and the 
 		# simulation of real data are close (i.e., smaller than some value epsilon)
-		if epsilon is None: epsilon = np.linalg.norm(summary)/8. # chosen quite arbitrarily
+		if epsilon is None: epsilon = np.linalg.norm(summary)/5. # chosen quite arbitrarily
 		accept_indices = np.argwhere(ro < epsilon)[:, 0]
 		reject_indices = np.argwhere(ro >= epsilon)[:, 0]
+
+		truths = [np.mean(real_data), np.std(real_data)**2]
+
 
 		# plot output samples and histogram of the accepted samples in 1D
 		def plot_samples_oneD():
@@ -468,12 +555,18 @@ class nholder(object):
 
 			ax[0, 0].set_title('Epsilon is chosen to be %.2f'%epsilon)
 			ax[0, 0].scatter(theta1[reject_indices], s[reject_indices, 0], s = 1, alpha = 0.1)
-			ax[0, 0].scatter(theta1[accept_indices] , s[accept_indices, 0], s = 1)
-			ax[0, 0].plot(prior[0], [summary[0], summary[0]], color = 'black', linestyle = 'dashed')
+			# plot 1 point again for the label
+			ax[0, 0].scatter(theta1[reject_indices][0], s[reject_indices, 0][0],c='C0' , s = 1, alpha = 1, label='Rejected')
+
+			ax[0, 0].scatter(theta1[accept_indices] , s[accept_indices, 0], s = 1,label='Accepted')
+			ax[0, 0].plot(prior[0], [summary[0], summary[0]], color = 'black', linestyle = 'dashed', label='Real data')
 			ax[0, 0].set_ylabel('Network output', labelpad = 0)
 			ax[0, 0].set_xlim(prior[0])
+			ax[0, 0].legend(frameon=False)
+
 			ax[1, 0].hist(theta1[accept_indices], bins=np.linspace(*prior[0], 100)
 				, histtype = u'step', density = True, linewidth = 1.5, color = '#9467bd');
+			ax[1, 0].axvline(truths[0],0,1,linestyle='dashed',c='blue')
 			ax[1, 0].set_xlabel('$\\theta_1$ (mean)')
 			ax[1, 0].set_ylabel('$\\mathcal{P}(\\theta|{\\bf d})$')
 			ax[1, 0].set_yticks([])
@@ -485,6 +578,7 @@ class nholder(object):
 			ax[0, 1].set_xlim(prior[1])
 			ax[1, 1].hist(theta2[accept_indices], np.linspace(*prior[1], 100)
 				, histtype = u'step', density = True, linewidth = 1.5, color = '#9467bd');
+			ax[1, 1].axvline(truths[1],0,1,linestyle='dashed',c='blue')
 			ax[1, 1].set_xlabel('$\\theta_2$ (variance)')
 			ax[1, 1].set_ylabel('$\\mathcal{P}(\\theta|{\\bf d})$')
 			ax[1, 1].set_yticks([])
@@ -499,7 +593,7 @@ class nholder(object):
 		def plot_samples_twoD():
 			hist_kwargs = {} # add kwargs to give to matplotlib hist funct
 			fig, ax = plt.subplots(2, 2, figsize = (10, 10))
-			fig = corner.corner(theta[accept_indices], fig=fig, truths = theta_fid
+			fig = corner.corner(theta[accept_indices], fig=fig, truths = truths
 				, labels=['$\\theta_1$ (mean)','$\\theta_2$ (variance)']
 				, plot_contours=True, range=prior, hist_kwargs=hist_kwargs)
 			fig.suptitle('Approximate posterior after ABC for %i draws'%draws)
@@ -508,7 +602,7 @@ class nholder(object):
 			plt.close()
 
 		if oneD == 'both':
-			plot_samples()
+			plot_samples_oneD()
 			plot_samples_twoD()
 		elif type(oneD) == bool:
 			if oneD: 
@@ -543,7 +637,7 @@ class nholder(object):
 		prior 					list 	lower and upper bounds for uniform priors
 		draws 					int 	number of initial draws from the prior
 		num_keep				int 	number of samples in the approximate posterior
-        criterion				float	ratio of number of draws wanted over number of draws needed
+		criterion				float	ratio of number of draws wanted over number of draws needed
 		show 					bool	whether or not plt.show() is called
 		oneD 					bool 	whether to plot one dimensional posteriors
 										or two dimensional with the corner module		
@@ -650,20 +744,21 @@ def generate_data(θ, train = None):
 
 # ALL PARAMETERS
 #_______________________________________________________
-input_shape = [10]
+input_shape = [100]
 theta_fid = np.array([0.,1.]) # mean and variance
-delta_theta = np.array([0.1,0.1]) # perturbation values
-n_s = 10000 # number of simulations
+delta_theta = np.array([0.10,0.10]) # perturbation values
+n_s = 1000 # number of simulations
 n_train = 1 # splits, for if it doesnt fit into memory
 # use less simulations for numerical derivative
-derivative_fraction = 0.05
+derivative_fraction = 0.2
 eta = 1e-5
-num_epochs = int(1e4)
+num_epochs = int(100e3)
 keep_rate = 0.6
 verbose = 0
 hidden_layers = [256,256,256]
-initial_version = 1013
+initial_version = 1034
 # Version < 1006 erroneously uses np.sqrt(cov) instead of cov in IMNN code
+# Version 1034 uses determinant of cov matrix as added to loss
 
 version = initial_version
 
@@ -686,18 +781,44 @@ nholder1 = nholder(input_shape, generate_data, theta_fid, delta_theta, n_s,
 		n_train, derivative_fraction, eta, parameters, num_epochs, keep_rate,
 		verbose, version)
 
+# # Rescale the data 
+#  ###### DO THIS BEFORE CALLING CREATE_NETWORK
+# nholder1.rescale_data()
+
 # # IMNN network
 n = nholder1.create_network()
+
+# Plot data
+nholder1.plot_data_hist(show=False)
+
+# Plot upper/lower
+nholder1.plot_derivatives(show=False)
+
 # # Train network
 nholder1.train_network(n)
 # # Plot the output
-nholder1.plot_variables(n,show=False)
+nholder1.plot_variables(n,show=True)
+# Plot output of 10 random inputs
+# nholder1.plot_train_output(n, show=True, amount=100)
+
 # # Perform ABC
 real_data = generate_data(np.array([theta_fid]), train = None)
-prior = [[-3, 3], [0,6]] # [ [prior1], [prior2], etc.. ]
-draws = 1000000
-nholder1.ABC(n, real_data, prior, draws, show=True, epsilon=None,oneD=False)
+prior = [[-3, 3], [0,3]] # [ [prior1], [prior2], etc.. ]
+draws = 100000
+
+print ('Real data mean, var: ', np.mean(real_data), np.std(real_data)**2)
+
+
+theta, _ = nholder1.ABC(n, real_data, prior, draws, show=False, epsilon=None,oneD='both')
+
+np.holdup()
 
 num_keep = int(1e3)
 inital_draws = int(1e4)
 nholder1.PMC_ABC(n, real_data, prior, inital_draws, num_keep, criterion = 0.1, show=True,oneD=False)
+
+"""
+nholder1.num_epochs = int(2e3)
+nholder1.train_network(n, to_continue=True)
+nholder1.plot_variables(n,show=True)
+"""

learning_purposes/randomfield/generate_randomfield.py
@@ -2,6 +2,11 @@ from __future__ import print_function, division
 import sys
 
 import numpy as np
+
+import matplotlib
+# matplotlib.use('agg')
+# print ('Using matplotlib "agg" mode')
+
 import matplotlib.pyplot as plt
 from scipy.special import erf
 
@@ -10,18 +15,6 @@ from scipy.special import erf
 From:
 http://cosmo.nyu.edu/roman/courses/comp_physics_2015/lecture_10.pdf
 """
-
-def fft_Indgen(n): # index generator
-	"""
-	Generates indices for size n
-	returns a list which is range(-n/2,n/2)
-	"""
-	a = range(0, n//2+1)
-	b = range(1, n//2)
-	b.reverse()
-	b = [-i for i in b]
-
-	return b + a
 
 """
 We start by generating the two-point function in Fourier space: phi(k)
@@ -35,7 +28,14 @@ A(k) = A(-k)
 B(k) = -B(-k)
 
 """
-def generate_phik(n=100, Pk = lambda k : k**1):
+def fftIndgen(n):
+		a = range(0, n//2+1)
+		b = range(1, n//2)
+		b.reverse()
+		b = [-i for i in b]
+		return a + b
+
+def generate_phik(n=100, alpha=1):
 	"""
 	Generates the two-point function phi(k) in Fourier space.
 
@@ -57,78 +57,274 @@ def generate_phik(n=100, Pk = lambda k : k**1):
 
 	A(k) = sqrt(P(k)/2) r cos(theta)
 	B(k) = sqrt(P(k)/2) r sin(theta)
+
+	returns 
+	phi_x -- the randomfield
 	"""
+	Pk = lambda k: k**alpha
 	A_k_array = np.zeros((n,n))
 	B_k_array = np.zeros((n,n))
+	# For each k, generate two uniform numbers u1 and u2
+	all_u1, all_u2 = np.random.uniform(size=(n,n)), np.random.uniform(size=(n,n))
 	all_k = np.zeros((n,n))
-	for i, kx in enumerate(fft_Indgen(n)):
-		for j, ky in enumerate(fft_Indgen(n)):
-			# For each k, generate two uniform numbers u1 and u2
-			u1 = np.random.uniform()
-			u2 = np.random.uniform()
-			# Find theta and r
-			theta = 2*np.pi*u1
-			r = np.sqrt(-2*np.log(1-u2))
+
+	# Find theta and r for all the uniform numbers
+	all_theta = 2*np.pi*all_u1
+	all_r = np.sqrt(-2*np.log(1-all_u2))
+
+	for i, kx in enumerate(fftIndgen(n)):
+		for j, ky in enumerate(fftIndgen(n)):
 
 			k = np.sqrt(kx**2+ky**2)
-			all_k[i,j] = k
+			all_k[i,j] = k # store all k
 
-			# Build the Fourier coefficients 
-			A_k_array[i,j] = np.sqrt(Pk(k)/2) * r * np.cos(theta)
-			B_k_array[i,j] = np.sqrt(Pk(k)/2) * r * np.sin(theta)
+			# only use half of Fourier space
+			if (ky > 0) or (ky==0 and kx>0): 
+				# since ky<0 is complex conjugates of those above
+				# and for ky=0 opposite kx must also be complex conjugates
+				
+				# For each k, generate two uniform numbers u1 and u2
+				# Find theta and r (see above the for loop)
 
-	# The fourier coefficients below (ky<0) are conjugates of those above (ky>0)
-	# For ky = 0, opposite k_x's are also complex conjugates
-	# kx = ky = 0 must be a real number, since its is own complex conjugate
+				# Build the Fourier coefficients 
+				A_k_array[i,j] = np.sqrt(Pk(k)/2) * all_r[i,j] * np.cos(all_theta[i,j])
+				B_k_array[i,j] = np.sqrt(Pk(k)/2) * all_r[i,j] * np.sin(all_theta[i,j])
 
+	# Build the complex conjugate fourier coefficients
+	for i, kx in enumerate(fftIndgen(n)):
+		for j, ky in enumerate(fftIndgen(n)):
+			"""
+			The fourier coefficients below (ky<0) are conjugates of those above (ky>0)
+			For ky = 0, opposite k_x's are also complex conjugates
+			kx = ky = 0 must be a real number, since its is own complex conjugate, 
+			this is fixed since we initialize the arrays with zeros
+			"""
+
+			if ky < 0:
+				# for ky < 0 opposite ky is complex conjugate, 
+				# i.e., A_(kx,ky) = A_(kx,-ky), B_(kx,ky) = -B_(kx,-ky)
+				A_k_array[i,j] = A_k_array[i,-j]
+				B_k_array[i,j] = -1 * B_k_array[i,-j]
+			
+			if ky == 0:
+				if kx < 0:
+					# ky = 0 opposite k_x are complex conjugate
+					A_k_array[i,j] = A_k_array[-i,j]
+					B_k_array[i,j] = -1 * B_k_array[-i,j]
+
+	phi_k = A_k_array + 1j*B_k_array
+	phi_k = np.absolute(phi_k)
+	plt.imshow(phi_k)
+	plt.xticks([])
+	plt.yticks([])
+	plt.title('Phi_k for Pk ~ k^%2.f, NOT SHIFTED TO CENTER = (0,0)'%alpha)
+	plt.show()
+#	# return phi_k
+
+
+	# To have the origin (kx,ky) = (0,0) in the center
+	# # phi_k = np.fft.fftshift(phi_k)
+	# But we don't want to do this
+
+	# Calculate the randomfield
 	phi_x = np.zeros((n,n))
-	for x in range(0,n):
-		print (x)
-		for y in range(0,n):
+	for x_index, x in enumerate(fftIndgen(n)): # x also goes from -49 to +50 in a 100,100 map
+		print ('Doing x=',x) # will have to do fftshift though to put x,y = 0,0 in the middle
+		for y_index, y in enumerate(fftIndgen(n)):
 			x_vec = np.array([x,y])
 			# sum over half of fourier space 
 			# seperate ky>0 from ky<0 and for ky=0 seperate kx<0 from kx>0
-			for i, kx in enumerate(fft_Indgen(n)):
-				for j, ky in enumerate(fft_Indgen(n)):		
+			for i, kx in enumerate(fftIndgen(n)):
+				for j, ky in enumerate(fftIndgen(n)):		
 					if ky > 0:
 						k_vec = np.array([kx,ky])
-						phi_x[x,y] += ( A_k_array[i,j] * np.cos(np.dot(k_vec,x_vec))
+						phi_x[x_index,y_index] += (A_k_array[i,j] * np.cos(np.dot(k_vec,x_vec))
 										 - B_k_array[i,j] * np.sin(np.dot(k_vec,x_vec)) )
 
 					if ky == 0:
 						if kx > 0:
-							phi_x[x,y] += (A_k_array[i,j] * np.cos(np.dot(k_vec,x_vec))
+							phi_x[x_index,y_index] += (A_k_array[i,j] * np.cos(np.dot(k_vec,x_vec))
 										 - B_k_array[i,j] * np.sin(np.dot(k_vec,x_vec)) )
 
 
-	phi_x *= 2
+	phi_x = 2 * np.fft.fftshift(phi_x)/phi_x.size # to put (x,y) = (0,0) in center of array
 	plt.imshow(phi_x,origin='lower')
 	plt.colorbar()
-	plt.title('Randomfield from Pk = k^1')
+	plt.title('Randomfield from Pk = k^%i'%alpha)
 	plt.xlabel('x')
 	plt.ylabel('y')
-	plt.savefig('./Figures/randomfield_CA.png')
+	plt.xticks([])
+	plt.yticks([])
+	plt.savefig('./Figures/randomfield.png')
+	# plt.show()
+	plt.close()
+
+	phi_x_ift = np.fft.ifft2(phi_k) # now we dont have to do FFT shift if I understand correctly
+	plt.imshow(phi_x_ift.real,origin='lower')
+	plt.colorbar()
+	plt.title('Randomfield from Pk = k^%i (IFFT.real)'%alpha)
+	plt.xlabel('x')
+	plt.ylabel('y')
+	plt.savefig('./Figures/randomfield_ifft.png')
+	# plt.show()
+	plt.close()
+
+	plt.imshow(phi_x_ift.imag,origin='lower')
+	plt.colorbar()
+	plt.title('Randomfield from Pk = k^%i (IFFT.imag)'%alpha)
+	plt.xlabel('x')
+	plt.ylabel('y')
+	plt.savefig('./Figures/randomfield_ifft_imag.png')
+	# plt.show()
+	plt.close()
+
+	return phi_x
+
+def createAnnularMask(dimx, dimy, center, big_radius, small_radius):
+	"""
+	From https://stackoverflow.com/questions/49837377/creating-ring-shaped-mask-in-numpy
+	Better to use the function radial_profile
+	"""
+
+	Y, X = np.ogrid[:dimx, :dimy]
+	distance_from_center = np.sqrt((X - center[0])**2 + (Y-center[1])**2)
+
+	mask = (small_radius <= distance_from_center) & \
+		(distance_from_center <= big_radius)
+	
+	return mask
+
+def radial_profile(data, center):
+	"""
+	Calculate radial profile of array 'data', given the center 'center'
+	"""
+	y, x = np.indices((data.shape))
+	r = np.sqrt((x - center[0])**2 + (y - center[1])**2)
+	r = r.astype(np.int)
+
+	tbin = np.bincount(r.ravel(), data.ravel())
+	nr = np.bincount(r.ravel())
+	radialprofile = tbin / nr
+	return radialprofile 
+
+def gaussian_random_field(Pk = lambda k : k**-3.0, size = 100):
+	"""
+	From http://andrewwalker.github.io/statefultransitions/post/gaussian-fields/
+	"""
+	def Pk2(kx, ky):
+		if kx == 0 and ky == 0:
+			return 0.0
+		return np.sqrt(Pk(np.sqrt(kx**2 + ky**2)))
+
+	noise = np.fft.fft2(np.random.normal(size = (size, size)))
+	amplitude = np.zeros((size,size))
+	kxky = np.zeros((size,size,2))
+	for i, kx in enumerate(fftIndgen(size)):
+		for j, ky in enumerate(fftIndgen(size)):			
+			kxky[i, j] = (kx,ky)
+			amplitude[i, j] = Pk2(kx, ky)
+
+	return np.fft.ifft2(noise * amplitude)
+
+def calculate_power_spectrum_from_grf(alpha= -3.0, size = 100):
+	"""
+	This function calculates the power spectrum from the function gaussian_random_field
+	"""
+	Pk = lambda k : k**alpha
+	grf = gaussian_random_field(Pk, size)
+	phi_x = grf.real
+	print ('Sum of randomfield',np.sum(grf))
+
+	# calculate the FT of the image
+	phi_k = np.fft.fft2(phi_x)
+	# only keep the amplitude, which is the square of the absolute
+	phi_k = np.absolute(phi_k)**2/(2*np.pi)**2
+
+	# shift the array such that the zero k-mode is in the center
+	# before shift, kx and ky are given by  [range(0,size/2+1) + range(-(size/2-1), 1)]
+										# e.g., ranging from 0 to 50 included + -49 to 0 included
+	phi_k = np.fft.fftshift(phi_k)
+	
+	# Then calculate the radial profile of phi_k around the center
+	center = (phi_k.shape[0]/2,phi_k.shape[1]/2)
+	Power_spectrum = radial_profile(phi_k, center)
+	all_k = np.arange(0,len(Power_spectrum),dtype='float')
+
+	fig, axarr = plt.subplots(1, 3, figsize=(16,6))
+	axarr[0].imshow(phi_x)
+	axarr[0].set_title('Randomfield generated from Pk = k^%i'%alpha)
+	axarr[0].set_xticks([])
+	axarr[0].set_yticks([])
+
+	axarr[1].imshow(np.log10(phi_k))
+	axarr[1].set_title('(log10 of) Amplitude of FT of randomfield')
+	axarr[1].set_xticks([])
+	axarr[1].set_yticks([])
+
+	theoretical = np.asarray(all_k)**alpha
+	# Should fix this later so that we dont have to do this
+	# intercept = Power_spectrum[0]
+
+	amplitude = 1 # np.mean(Power_spectrum/theoretical)
+
+	# print ('Amplitude is not correct yet, normalizing by..',amplitude)
+	# print ('Std of amplitude wrt theoretical',np.std(Power_spectrum/theoretical))
+	# print ('intercept as well, adding ',intercept)
+
+	# Should fix this later so that we have the same amplitude and intercept
+	theoretical = amplitude*np.asarray(all_k)**alpha
+	# start at 1 because k = 0, Power = 0
+	axarr[2].plot(all_k[1:],Power_spectrum[1:])
+	axarr[2].plot(all_k[1:], theoretical[1:]
+		,label='Pk = %.1f k**%.1f'%(amplitude,alpha), ls='dashed')
+	axarr[2].set_xlabel('k')
+	axarr[2].set_ylabel('P(k)')
+	axarr[2].set_title('Power spectrum inferred')
+	axarr[2].legend()
+	axarr[2].set_yscale('log')
 	plt.show()
-	plt.close()
-
-	phi_x_test = np.fft.ifft(A_k_array + 1j*B_k_array)
-	plt.imshow(phi_x_test.real,origin='lower')
-	plt.colorbar()
-	plt.title('Randomfield from Pk = k^1 (IFFT.real)')
-	plt.xlabel('x')
-	plt.ylabel('y')
-	plt.savefig('./Figures/randomfield_CA_ifft.png')
-	# plt.show()
-	plt.close()
-
-	plt.imshow(phi_x_test.imag,origin='lower')
-	plt.colorbar()
-	plt.title('Randomfield from Pk = k^1 (IFFT.imag)')
-	plt.xlabel('x')
-	plt.ylabel('y')
-	plt.savefig('./Figures/randomfield_CA_ifft_imag.png')
-	# plt.show()
-	plt.close()
 
 if __name__ == '__main__':
-	generate_phik(n=50)
+	alpha = -3
+	n = 200
+	calculate_power_spectrum_from_grf(alpha,n)
+
+	"""
+	phi_x = generate_phik(n=n, alpha= alpha)
+
+	phi_k = np.fft.fft2(phi_x)
+	phi_k = np.absolute(phi_k)
+
+	# fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(14, 6))
+	# ax[0,0].hist(phi_k.ravel(), bins=100)
+	# ax[0,0].set_title('hist(|phi_k|)')
+	# ax[0,1].hist(np.log(phi_k).ravel(), bins=100)
+	# ax[0,1].set_title('hist(log(|phi_k|))')
+	# im2 = ax[1,0].imshow(phi_x, interpolation="none")
+	# ax[1,0].set_title('Randomfield')
+	# im1 = ax[1,1].imshow(np.log(phi_k), interpolation="none")
+	# ax[1,1].set_title('log(|phi_k|)')
+	# plt.colorbar(im1)
+	# plt.show()
+
+	# then make concentric rings of radius K and width dK to calculate P(k)
+	# k runs from -x/2 to x/2
+	Pk = []
+	k = []
+	dk = 1.0
+	center = (phi_k.shape[0]/2,phi_k.shape[1]/2)
+	for i in range(0,26):
+		small_radius = i
+		big_radius = small_radius+dk
+		annularmask = createAnnularMask(phi_k.shape[0],phi_k.shape[1]
+			,center,big_radius,small_radius)
+		# masked_array = np.ma.array(phi_k, mask=annularmask^1)
+		Pk.append(np.sum(phi_k[annularmask]))
+		k.append(i)
+
+	plt.plot(k,Pk)
+	plt.xlabel('k')
+	plt.ylabel('Pk')
+	plt.title('Power spectrum inferred from randomfield generated from Pk = k^%i'%alpha)
+	plt.show()
+	"""

utils_mrp.py
@@ -43,6 +43,7 @@ def save_model_settings(nholder, settings):
 	"""
 
 	file = nholder.modelsettings_name
+	print (f'Saving modelsettings to {file}')
 	# open the previous modelsettings.csv file
 	try:
 		csv_settings = pd.read_csv(file)
@@ -57,7 +58,12 @@ def save_model_settings(nholder, settings):
 	
 	# Append to end of file
 	appendloc = len(csv_settings) 
-	csv_settings.loc[appendloc] = appenddata
+	try:
+		csv_settings.loc[appendloc] = appenddata
+	except ValueError:
+		print (f'{file} has incorrect columns')
+		print ('Trying to set ', appenddata)
+		raise ValueError('Please change your modelsettings.csv file to accommodate the correct column names, or delete the file')
 	# Write 
 	csv_settings.to_csv(file, index = False)
 
@@ -70,12 +76,24 @@ def save_final_fisher_info(nholder, n):
 	file = nholder.modelsettings_name
 	# open the modelsettings.csv file
 	csv_settings = pd.read_csv(file)
-	# the row of the current version
+	# Likely the row of the current version
 	current_row = len(csv_settings)-1
-	assert csv_settings['Version'][current_row] == nholder.modelversion
+	# make sure it is indeed the current version
+	if csv_settings['Version'][current_row] == nholder.modelversion:
+		pass
+	else:
+		print ("Another network was likely finished before this run.")
+		print ("Finding the most recent version of the run")
+		print ("Please check manually if this did not overwrite anything")
+		current_row = np.where(csv_settings['Version'][::-1] == nholder.modelversion)[0][0]
 
-	csv_settings['Final detF train'][current_row] = '{0:.2f}'.format(n.history["det(F)"][-1])
-	csv_settings['Final detF test'][current_row] = '{0:.2f}'.format(n.history["det(test F)"][-1])
+	# These two lines give a VIEW vs COPY warning
+	# csv_settings['Final detF train'][current_row] = '{0:.2f}'.format(n.history["det(F)"][-1])
+	# csv_settings['Final detF test'][current_row] = '{0:.2f}'.format(n.history["det(test F)"][-1])
+
+	# Proper practice seems to be the following syntax
+	csv_settings.loc[current_row,('Final detF train')] = '{0:.2f}'.format(n.history["det(F)"][-1])
+	csv_settings.loc[current_row,('Final detF test') ] = '{0:.2f}'.format(n.history["det(test F)"][-1])	
 
 	csv_settings.to_csv(file, index = False)
 