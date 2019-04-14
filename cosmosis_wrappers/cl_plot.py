6e7958fbdc21f380623f478d0592376084f886c6
Erik Osinga <osinga@reusel.strw.leidenuniv.nl>
2019-04-14 18:28:56 +0200

    implemented loading from file

--- cosmosis_wrappers/cl_plot.py
+++ cosmosis_wrappers/cl_plot.py
@@ -10,26 +10,25 @@ def plot_cosmosis_cells(data_dir,bins):
 	ell = np.loadtxt(f'{data_dir}/ell.txt')
 	
 	fig = plt.figure(figsize=(10,8))
-	for i in range(1,bins+1): # Cosmosis starts numbering at 1
-		for j in range(1,bins+1):
-			if i >= j: 
-				c_ell = np.loadtxt(f'{data_dir}/bin_{i}_{j}.txt')
-				y_axis = ell*(ell+1)*c_ell / (2*np.pi)
+	for j in range(1,bins+1): # 
+		for i in range(j,bins+1): # Cosmosis starts numbering at 1
+			c_ell = np.loadtxt(f'{data_dir}/bin_{i}_{j}.txt')
+			y_axis = ell*(ell+1)*c_ell / (2*np.pi)
 
-				ax = plt.subplot2grid((bins+1,bins+1), (i,j-1))
-				
-				# For the label
-				plt.scatter(ell[0], y_axis[0], c='white',label=f'({i},{j})')
-				
-				plt.plot(ell, y_axis)
-				plt.legend(frameon=False)
-				plt.xscale('log')
-				plt.yscale('log')
+			ax = plt.subplot2grid((bins+1,bins+1), (i,j-1))
+			
+			# For the label
+			plt.scatter(ell[0], y_axis[0], c='white',label=f'({i},{j})')
+			
+			plt.plot(ell, y_axis)
+			plt.legend(frameon=False)
+			plt.xscale('log')
+			plt.yscale('log')
 
-				if (i, j) == (1,1):
-					plt.ylabel(r'$\ell*(\ell+1)*C_\ell / (2\pi)$')
-				if (i,j) == (bins,1):
-					plt.xlabel(r'$\ell$')
+			if (i, j) == (1,1):
+				plt.ylabel(r'$\ell*(\ell+1)*C_\ell / (2\pi)$')
+			if (i,j) == (bins,1):
+				plt.xlabel(r'$\ell$')
 
 	plt.subplots_adjust(wspace = 0.4, hspace = 0.4)
 	plt.tight_layout()
